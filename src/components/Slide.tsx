ed51e6c0d58edb8b625507315383c9016ea7af24
Adeel <faridiadeel5@gmail.com>
2022-05-09 07:19:45 +0530

    Task

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -4,7 +4,7 @@ import { SlideProps } from "./types";
 
 const Slide:FC<SlideProps> = ({ year, heading,bgColor }) => {
   const primaryStyle:string = "flex-center flex-1 h-100 bg-dark p-10";
-  const yellowBackground:string = ` ${bgColor}`;
+  const bgColorProp:string = ` ${bgColor}`;
   const transparentBackground:string = " bg-transparent";
 
   const getClasses = (overwrite:string) => {
@@ -18,7 +18,7 @@ const Slide:FC<SlideProps> = ({ year, heading,bgColor }) => {
       <div {...getClasses(transparentBackground)}>
         {heading && <h1 className="heading f-40">Deep Dive into our Annual Reports</h1>}
       </div>
-      <div {...getClasses(yellowBackground)}>
+      <div {...getClasses(bgColorProp)}>
         <div className="flex-center h-full flex-col">
           <h1 className="heading f-48">{year}</h1>
           <button className="btn-primary">Download Report</button>
